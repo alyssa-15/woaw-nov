e8efbabb4471846c09d2ab369efbb4de884e8ae9
Alyssa <alyssa.flynn1510@gmail.com>
2018-11-04 14:33:41 +1000

    home page is complete

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -2,28 +2,31 @@ cssVars();
 
 // open nav
 
-$('.nav-prompt').click(function() {
+$(".nav-prompt").click(function() {
+  // if nav inner attribute aria is false, do something
+  if ($(".nav-inner").attr("aria-expanded") == "false") {
+    $(".nav-inner").attr("aria-expanded", "true");
+  } else {
+    $(".nav-inner").attr("aria-expanded", "false");
+  }
 
-    // if nav inner attribute aria is false, do something
-    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+  if ($(".nav-prompt").text() == "Open Navigation") {
+    $(".nav-prompt").text("Close Navigation");
+  } else {
+    $(".nav-prompt").text("Open Navigation");
+  }
+});
 
-        $('.nav-inner').attr('aria-expanded', 'true');
+// nav active class
 
-    } else {
+function activeMenu() {
+  var url = window.location.href;
 
-        $('.nav-inner').attr('aria-expanded', 'false');
+  $(".nav-inner a")
+    .filter(function() {
+      return this.href == url;
+    })
+    .addClass("active");
+}
 
-
-    }
-
-    if ($('.nav-prompt').text() == 'Open Navigation') {
-
-        $('.nav-prompt').text('Close Navigation')
-
-    } else {
-
-        $('.nav-prompt').text('Open Navigation')
-
-    }
-
-})
+activeMenu();
