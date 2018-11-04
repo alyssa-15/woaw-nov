a3fc7560c758aa03f6af82432adc17411ce3230c
Alyssa <alyssa.flynn1510@gmail.com>
2018-11-04 12:23:54 +1000

    favicons and nav js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,1 +1,29 @@
 cssVars();
+
+// open nav
+
+$('.nav-prompt').click(function() {
+
+    // if nav inner attribute aria is false, do something
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+
+        $('.nav-inner').attr('aria-expanded', 'true');
+
+    } else {
+
+        $('.nav-inner').attr('aria-expanded', 'false');
+
+
+    }
+
+    if ($('.nav-prompt').text() == 'Open Navigation') {
+
+        $('.nav-prompt').text('Close Navigation')
+
+    } else {
+
+        $('.nav-prompt').text('Open Navigation')
+
+    }
+
+})
